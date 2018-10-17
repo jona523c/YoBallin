f093cbb22cf6cc29930b98437f6ffa8bb6a16d4c
Jonas Madsen <201708090@post.au.dk>
2018-10-17 20:17:37 +0200

    Start

---
+++ app/src/main/java/standard/ballin/MainActivity.java
@@ -0,0 +1,19 @@
+package standard.ballin;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    /** Called when user clicks play button **/
+    public void playButton(View view) {
+        // Do something
+    }
+}
