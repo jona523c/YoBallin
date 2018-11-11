a148e74f4814ad14ab0498e0ea163e8b42f8459e
Jonas Madsen <201708090@post.au.dk>
2018-11-11 16:28:12 +0100

    Settings and sound toggle added

--- app/src/main/java/standard/ballin/MainActivity.java
+++ app/src/main/java/standard/ballin/MainActivity.java
@@ -4,14 +4,34 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
+import android.widget.*;
 
 public class MainActivity extends AppCompatActivity {
+    ImageView soundButton;
+    ImageView settingsButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        soundButton = (ImageView) findViewById(R.id.soundView);
+        soundButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                soundButton.setActivated(!soundButton.isActivated());
+            }
+        });
+
+        settingsButton = (ImageView) findViewById(R.id.settingsView);
+        settingsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent (MainActivity.this, SettingsActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
     /** Called when user clicks play button **/
---
+++ app/src/main/java/standard/ballin/SettingsActivity.java
@@ -0,0 +1,18 @@
+package standard.ballin;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+    }
+
+    public void backButton(View view) {
+        finish();
+    }
+}
